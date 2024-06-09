631ec0e89b9da01f014c2ffd021b94ee31829471
shaodado <shaoshineshine@gamil.com>
2024-06-09 15:27:57 +0800

    refactor(service): 利用 inject 取得產品資料服務

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../model/product';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
-import { ProductService } from '../services/product.service';
+import { ProductService } from './../services/product.service';
 
 @Component({
   selector: 'app-product-page',
@@ -14,12 +14,11 @@ import { ProductService } from '../services/product.service';
 export class ProductPageComponent implements OnInit {
   router = inject(Router);
 
-  productService!: ProductService;
+  private productService = inject(ProductService);
 
   products!: Product[];
 
   ngOnInit(): void {
-    this.productService = new ProductService();
     this.products = this.productService.getList();
   }
 
