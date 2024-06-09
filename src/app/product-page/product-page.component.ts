b24128497613477ccb8eea01b37baa83eb753c33
shaodado <shaoshineshine@gamil.com>
2024-06-09 16:10:23 +0800

    feat(product-page):　新增產品功能

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -22,6 +22,21 @@ export class ProductPageComponent implements OnInit {
     this.products = this.productService.getList();
   }
 
+  onAdd(): void {
+    const product = new Product({
+      id: 1,
+      name: '產品 Z',
+      authors: ['作者A', '作者B', '作者C'],
+      company: '博碩文化',
+      isShow: true,
+      createDate: new Date(),
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 10000,
+    });
+
+    this.productService.add(product);
+  }
+
   onShop(product: Product): void {
     alert('已加入購物車');
   }
